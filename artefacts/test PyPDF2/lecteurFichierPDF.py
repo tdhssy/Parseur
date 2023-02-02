e708cecc6979e5769018957540bdd5b86fff2fd8
LuffyVanquish <luffyvanquish@gmail.com>
2023-02-02 13:54:06 +0100

    réorganisation des fichiers

---
+++ artefacts/test PyPDF2/lecteurFichierPDF.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from PyPDF2 import PdfReader
+
+"""
+Fonction permettant la lecture du document PDF
+et la récupération des informations de celui-ci.
+"""
+def readerPDF(fichier):
+    print("Analyse de " + fichier + " en cours ..." )
+    
+    #Lecture du fichier pdf
+    try:
+        lecteur = PdfReader(fichier)
+    except:
+        print(fichier + " introuvable")
+        return 
+    
+    #Récupération des informations du fichier
+    info = lecteur.metadata
+    titre = info.title
+    auteurs = info.author
+
+    #Récupération du texte
+    #TODO Distinguer les différents paragraphes.
+    nb_page = len(lecteur.pages)
+    page = lecteur.pages[0]
+    text = page.extract_text()
+    print(text)
