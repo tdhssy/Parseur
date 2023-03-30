d466e92b8556ae546820958632f89638f6c675ec
LuffyVanquish <luffyvanquish@gmail.com>
2023-03-30 17:41:24 +0200

    Amélioration de l'interface Gestion de cas d'erreur

--- parseur.py
+++ parseur.py
@@ -18,19 +18,31 @@ def convert(selectfic):
     #print("option :"+option)
     if option =="-t": 
         txt_writer(chemin + selectfic) #VERSION TXT 
-        print("-t " + chemin +  selectfic)
+        #print("-t " + chemin +  selectfic)
     elif option =="-x":  
         xml_writer(chemin + selectfic) #VERSION XML
-        print("-x " + selectfic)
+        #print("-x " + selectfic)
     elif option =="-tx " or option == "-xt":  
         xml_writer(chemin + selectfic) #VERSION XML
         txt_writer(chemin + selectfic) #VERSION TXT
-        print("-tx " + selectfic)
+        #print("-tx " + selectfic)
     else :
         print("usage: [type] [nomFichier]\n")
         print(" type:       -t -> sortie txt\n      -x -> sortie xml\n")
         print(" Rappel: Vous pouvez mettre plusieurs fichiers a la suite")
 
+
+def saisie(L):
+    inp = input("saisir un chiffre dans la liste ou * : ")
+    if inp == "*":
+        for i in  L :
+            convert(i)
+    elif int(inp) >0 and int(inp)<len(L)+1 : 
+        selectfic = L[int(inp)-1]
+        convert(selectfic)
+    else:
+        print("Erreur veuillez choisir parmis les propositions ")
+        saisie(L)
 
 if __name__ == '__main__':
     argc = len(sys.argv)
@@ -46,38 +58,25 @@ if __name__ == '__main__':
     fich = os.listdir(chemin)
 
     for i in fich:
+        #Verification du format du fichier
+        verifPDF = i.split(".")[-1] #Récupère seulement l'extension
+        try:
+            if verifPDF != "pdf":
+                raise Exception
+        except:
+            print(i + " n'est pas au format pdf")
+            continue
 
-
+        #Rajout dans la liste de fichiers
         L.append(i)
 
         
-
+    #Affichage de menu de selection
     for j in range( len(L)):
         print(str(j+1) + ". " + L[j])
     
-    print("13. *")
-
-    inp = input("saisir un chiffre dans la liste ")
-    if inp == "*":
-        for i in  L :
-            convert(i)
-    elif int(inp) >0 and int(inp)<len(L)+1 : 
-        selectfic = L[int(inp)-1]
-        convert(selectfic)
-    
-     
-    #Verification du format du fichier
-    #    verifPDF = selectfic.split(".")[-1] #Récupère seulement l'extension
-    #    try:
-    #        if verifPDF != "pdf":
-    #            raise Exception
-    #    except:
-    #        print(selectfic + " n'est pas au format pdf")
-    #        continue
-
-
-        #Analyse du fichier pdf 
-
+    print("*. Tout les fichiers")
+    saisie(L)
 
 
         
