89b78a474ef46c08e6529e64584b31a5aeb361c4
Antoine <fetiveau.antoine@gmail.com>
2023-05-12 10:28:28 +0200

    Amélioration biblio

--- extractionBiblio.py
+++ extractionBiblio.py
@@ -35,9 +35,29 @@ def recuperationBiblio(pages) -> str:
                 res += ligne + "\n"
             trouver = True
         cpt += 1
+
+    #Si on ne trouve rien on recherche une autre mot clé: biblio
+    if (not trouver):
+        cpt=0
+        while (cpt < nb_pages) and (not trouver):
+            page = pages[cpt]
+
+            try:
+                biblio = re.findall(r'(?i)[\n][0-9].\sbiblio[A-Za-z]*\sreference[s]?[\s]*[\n][\s\S]*',
+                                    ("\n" + page))[0].split('\n') #Ajout de \n au cas ou References est en début de page
+                biblio.pop(1)  #Pour enlever le mot clé "References"
+                biblio.pop(-1) #Pour enlever le numéro de la page
+            except:
+                biblio = []
+
+            if biblio != []:
+                for ligne in biblio:
+                    res += ligne + "\n"
+                trouver = True
+            cpt += 1
     
     #Une fois la partie Référence trouver on ajoute toute la fin du pdf
-    #car la partie Référence est la dernière partie d'un pdf.
+    #car la partie Référence est la dernière partie d'un pdf. (en général)
     while cpt < nb_pages:
         page = pages[cpt]
         for ligne in page.split('\n'):
