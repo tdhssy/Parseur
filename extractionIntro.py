188280e24c4b78c9e3e361be7e9f7a2213138cf5
TheoGJ <theoguennecjacques@gmail.com>
2023-05-12 11:37:15 +0200

    amélioration abstract

--- extractionIntro.py
+++ extractionIntro.py
@@ -11,7 +11,7 @@ def recuperationIntro(pages)->str:
     # Le texte que nous allons analyser
 
     # La regex qui correspond à ce qui est entre "Introduction" et "2."
-    regex = r"(I\s*n\s*t\s*r\s*o\s*d\s*u\s*c\s*t\s*i\s*o\s*n\s*|I\s*N\s*T\s*R\s*O\s*D\s*U\s*C\s*T\s*I\s*O\s*N\s*)\n([\s\S]*?)\n\s*(II|2)\.*\s"
+    regex = r"(?i)introduction\n([\s\S]*?)\n\s*(II|2)\.*\s"
  
     # Utilisation de la fonction search pour trouver la première occurrence de la regex dans la chaîne texte
     correspondance = re.search(regex, text)
